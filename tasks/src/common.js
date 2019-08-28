619acaa57322100b59326c68daab622a8cf76e9b
jose-guilherme-ti <51062796+jose-guilherme-ti@users.noreply.github.com>
2019-08-28 14:34:39 -0300

    Configura porta do Heroku

--- tasks/src/common.js
+++ tasks/src/common.js
@@ -2,9 +2,9 @@ import { Alert, Platform } from 'react-native';
 
 
 const server = Platform.OS === 'ios' ?
-    'http://10.9.6.93:3000' : 'http://10.9.6.93:3000';
+   // 'http://10.9.6.93:3000' : 'http://10.9.6.93:3000';
    //'http://10.0.3.2:3000' : 'http://10.0.3.2:3000';
-
+    'https://apifindcode.herokuapp.com' : 'https://apifindcode.herokuapp.com';
 function showError(err) {
     Alert.alert('Ops! Ocorreu um Problema!', `Mensagem:${err}`)
 }
